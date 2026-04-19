76d9caa02b2444139b9fee5e7126803cc6f018b4
Suthar2005meet <sm6642223@gmail.com>
2026-04-19 13:42:35 +0530

    feat: initialize Express server with CORS, database connection, and user/task routing

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-  origin: "https://your-frontend.vercel.app/"
+    origin: "https://tododolist.vercel.app"
 }));
 
 const DBConnection = require('./src/Config/DBConnection')
